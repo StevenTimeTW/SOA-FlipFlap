4a30759344b6a70d2236a43f620a8e0b0dfd7f68
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-10-06 16:32:48 +0800

    Pass all the tests - Shameless Green

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,17 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    rows = tsv.split("\n").map{ |row| row.split("\t")}
+    headers = rows.first.map(&:chomp)
+    data_rows = rows[1..]
+    @data = data_rows.map{|row| headers.zip(row.map(&:chomp)).to_h}
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    col_names = @data[0].keys.join("\t") + "\n"
+    values = @data.map{|row| row.values.join("\t")}.join("\n") + "\n"
+    col_names + values
   end
 end
