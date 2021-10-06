b57662d07730eeaebb4ba75183bb6d21bc1fdfde
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-10-06 16:40:02 +0800

    Revise Style using Rubocop

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-    rows = tsv.split("\n").map{ |row| row.split("\t")}
+    rows = tsv.split("\n").map { |row| row.split("\t") }
     headers = rows.first.map(&:chomp)
     data_rows = rows[1..]
-    @data = data_rows.map{|row| headers.zip(row.map(&:chomp)).to_h}
+    @data = data_rows.map { |row| headers.zip(row.map(&:chomp)).to_h }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-    col_names = @data[0].keys.join("\t") + "\n"
-    values = @data.map{|row| row.values.join("\t")}.join("\n") + "\n"
+    col_names = "#{@data[0].keys.join("\t")}\n"
+    values = "#{@data.map { |row| row.values.join("\t") }.join("\n")}\n"
     col_names + values
   end
 end
